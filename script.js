6fb78775819e6b2150f65cd511830b0d2d8f6baa
Ted Whang <taeywhang@gmail.com>
2014-04-27 01:05:07 -0700

    Ball on initial display and add arrow pointing which direction ball will go.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-var direction = 'right';
 var triCount = 1;
 var gameNumber = 1;
-var game1 = [[360, 80],['bottomRight', '200', '200'], ['topLeft', '0', '80'], ['bottomRight', '0', '200'], ['topRight', '200', '0']];
-var ballX = 0;
-var ballY = 0;
-var adjX = 0;
-var adjY = 0;
-var winX = game1[0][0];
-var winY = game1[0][1];
+var ballSizeRatio = 40;
+var game1 = [[9, 2, 0, 0, 'right'],['bottomRight', 5, 5], ['topLeft', 0, 2], ['bottomRight', 0, 5], ['topRight', 5, 0]];
+var game2 = [[9, 2, 1, 9, 'up'], ['topLeft', 2, 0], ['topRight', 4, 0], ['bottomLeft', 4, 2]]
+var winX = game1[0][0] * ballSizeRatio;
+var winY = game1[0][1] * ballSizeRatio;
 var cont = true;
 var launch = false;
 var game;
 var ball;
-
 
 var triArray = [['topLeft', 0], ['topRight', 1], ['bottomRight', 2], ['bottomLeft', 3]];
 var dirHash = {'right': 0, 'down': 1, 'left': 2, 'up': 3}
@@ -27,31 +23,43 @@ $(document).ready(function() {
 });
 
 $(document).on('keydown', function(event) {
-  if(event.which == 78) {
-    game = new Game();
-    game.loadLevel();
-  } else if(event.which == 32 && !launch) {
-    launch = true;
-    ball = new Ball();
-    ball.drawBall();
-  }
 });
 
-function Ball() {
-  this.drawBall = function() {
-    $('#gameBoard').prepend('<div class="ball"></div>');
-    this.moveBall();
+function start() {
+  $('.start').addClass('hide');
+  $('.launch').removeClass('hide');
+  game = new Game();
+  game.loadLevel();
+  ball = new Ball();
+  ball.drawBall();
+}
 
+function launchBall() {
+  $('.launch').addClass('hide');
+  $('.arrow').addClass('hide');
+  launch = true;
+  ball.moveBall(); 
+}
+
+function Ball() {
+  this.direction = 'right';
+  this.ballX = 0;
+  this.ballY = 0;
+  this.adjX = 0;
+  this.adjY = 0;
+
+  this.drawBall = function() {
+    $('#gameBoard').prepend('<div class="arrow arrow-' + game1[0][4] + '"></div><div class="ball"></div>');
   },
 
   this.moveBall = function() {
     this.touch();
     if (cont) {
-      if (direction == 'right') {
+      if (this.direction == 'right') {
         this.move('right', 40);
-      } else if(direction == 'down') {
+      } else if(this.direction == 'down') {
         this.move('down', 40);
-      } else if(direction == 'left') {
+      } else if(this.direction == 'left') {
         this.move('left', -40);
       } else {
         this.move('down', -40);
@@ -61,21 +69,22 @@ function Ball() {
   },
 
   this.touch = function() {
-    if (ballX == winX && ballY == winY) {
+    if (this.ballX == winX && this.ballY == winY) {
       cont = false;
       $('.ball').animate({height: '+= 0px'}, function() {
+        $('.ball').css({margin: '5px', height: '30px', width: '30px'}); 
         $('.win').removeClass('hide');
         $('.end').css('background', 'red');
         console.log(cont);
       });
-    } else if (ballX > 360 || ballX < 0 || ballY > 360 || ballY < 0) {
+    } else if (this.ballX > 360 || this.ballX < 0 || this.ballY > 360 || this.ballY < 0) {
       cont = false;
       $('.ball').animate({height: '+= 0px'}, function() {
         $('.lose').removeClass('hide');
       });
     } else {
       for(var i = 1; i < game1.length; i++) {
-        if (adjX == game1[i][1] && adjY == game1[i][2]) {
+        if (this.adjX == game1[i][1]*ballSizeRatio && this.adjY == game1[i][2]*ballSizeRatio) {
           this.changeDirection(i);
         }
       }
@@ -84,23 +93,23 @@ function Ball() {
 
   this.move = function(direction, units) {
     if (direction == 'right' || direction == 'left') {
-      ballX += units;
-      adjX = ballX + units;
-      adjY = ballY;
-      $('.ball').animate({left: '+=' + units + 'px'}, 100);
+      this.ballX += units;
+      this.adjX = this.ballX + units;
+      this.adjY = this.ballY;
+      $('.ball').animate({left: '+=' + units + 'px'}, 50);
     } else {
-      ballY += units;
-      adjX = ballX;
-      adjY = ballY + units;
-      $('.ball').animate({top: '+=' + units + 'px'}, 100);
+      this.ballY += units;
+      this.adjX = this.ballX;
+      this.adjY = this.ballY + units;
+      $('.ball').animate({top: '+=' + units + 'px'}, 50);
     }
   },
 
   this.changeDirection = function(i) {
     for (var j = 0; j < triArray.length; j++) {
       if ($('.tri' + i).hasClass(triArray[j][0])) {
-        this.move(direction, cTri[j][dirHash[direction]][1]);
-        direction = cTri[j][dirHash[direction]][0];
+        this.move(this.direction, cTri[j][dirHash[this.direction]][1]);
+        this.direction = cTri[j][dirHash[this.direction]][0];
       }
     }
   }
@@ -111,14 +120,13 @@ function Game() {
     // Adds triangles
     for (var i = 1; i < game1.length; i++) {
       var tri = new Triangle();
-      tri.add(game1[i][0], game1[i][1], game1[i][2]);
+      tri.add(game1[i][0], (game1[i][1] * ballSizeRatio), (game1[i][2]) * ballSizeRatio);
     }
     
     // Adds End spot
     $('#gameBoard').prepend('<div class="end"></div>');
     $('.end').css('left', winX + 'px').css('top', winY + 'px');
-    
-  },
+  }
 }
 
 function Triangle() {
